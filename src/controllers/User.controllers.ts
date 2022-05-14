209496bb40a40ce6fa4fbb5f395c81ea7a6bd58c
SGman98 <bastiangarnica@gmail.com>
2022-05-14 16:47:42 -0500

    Update User controller

--- src/controllers/User.controllers.ts
+++ src/controllers/User.controllers.ts
@@ -1,111 +1,67 @@
 import { User } from "@models/User.model";
 import { Role } from "@models/Role.model";
 import { UserAddAttributes } from "@models/User.model";
+import { responseJson, controllerWrapper } from "@helpers/controller.helpers";
 
-export const getAllUsers = async (req: any, res: any) => {
-	try {
-		const users = await User.findAll({ include: [{ model: Role }] });
-		return res.status(200).json({
-			success: true,
-			message: "Users retrieved successfully",
-			data: users,
-		});
-	} catch (error: any) {
-		return res.status(500).json({
-			success: false,
-			message: error.message,
-			data: error,
-		});
+export const getAllUsers = controllerWrapper(async (_req: any, res: any) => {
+	const user = await User.findAll({ include: [{ model: Role }] });
+	return responseJson(res, 200, "User retrieved successfully", user);
+});
+
+export const createUser = controllerWrapper(async (req: any, res: any) => {
+	const userAtt: UserAddAttributes = { ...req.body, roleId: undefined };
+	const roleId: number = req.body.roleId;
+	const user = await User.create(userAtt, { include: [{ model: Role }] });
+	await user.$add("role", roleId);
+	return responseJson(res, 201, "User created successfully", user);
+});
+
+export const getUser = controllerWrapper(async (req: any, res: any) => {
+	const user = await User.findByPk(req.params.id, {
+		include: [{ model: Role }],
+	});
+	if (!user) return responseJson(res, 404, "User not found", null);
+	return responseJson(res, 200, "User retrieved successfully", user);
+});
+
+export const updateUser = controllerWrapper(async (req: any, res: any) => {
+	const userId: string = req.params.id;
+	const user: Partial<UserAddAttributes> = { ...req.body, roleId: undefined };
+	let roleId: number | number[] = req.body.roleId;
+
+	const userToUpdate = await User.findByPk(userId);
+	if (!userToUpdate) return responseJson(res, 404, "User not found", null);
+
+	const updatedUser = await userToUpdate.update(user);
+
+	if (roleId) {
+		// if roleId is not an array, make it an array
+		if (!Array.isArray(roleId)) roleId = [roleId];
+
+		const userRoles = await userToUpdate.$get("roles");
+
+		// remove roles that are not in the roleId array
+		for (const userRole of userRoles) {
+			if (!roleId.includes(userRole.id)) {
+				await userToUpdate.$remove("role", userRole.id, { force: true });
+			}
+		}
+
+		// for each role if not exist add it
+		for (const role of roleId) {
+			if (!(await userToUpdate.$has("role", role))) {
+				await userToUpdate.$add("role", role);
+			}
+		}
 	}
-};
 
-export const createUser = async (req: any, res: any) => {
-	try {
-		const userAtt: UserAddAttributes = { ...req.body, roleId: undefined };
-		const roleId: number = req.body.roleId;
-		const user = await User.create(userAtt);
-		await user.$add("role", roleId);
-		return res.status(201).json({
-			success: true,
-			message: "User created successfully",
-			data: user,
-		});
-	} catch (error: any) {
-		return res.status(500).json({
-			success: false,
-			message: error.message,
-			data: error,
-		});
-	}
-};
+	return responseJson(res, 200, "User updated successfully", updatedUser);
+});
 
-export const getUser = async (req: any, res: any) => {
-	try {
-		const user = await User.findByPk(req.params.id, {
-			include: [{ model: Role }],
-		});
-		if (!user) throw new Error("User not found");
-		return res.status(200).json({
-			success: true,
-			message: "User retrieved successfully",
-			data: user,
-		});
-	} catch (error: any) {
-		return res.status(500).json({
-			success: false,
-			message: error.message,
-			data: error,
-		});
-	}
-};
-
-export const updateUser = async (req: any, res: any) => {
-	try {
-		const userId: string = req.params.id;
-		const user: Partial<UserAddAttributes> = { ...req.body, roleId: undefined };
-		const roleId: number = req.body.roleId;
-
-		// update user there is only one user with this id
-		const [count, updatedUsers] = await User.update(user, {
-			where: { id: userId },
-			returning: true,
-			individualHooks: true,
-		});
-		if (count === 0) throw new Error("User not found");
-		if (count > 1) throw new Error("More than one user with this id"); // should never happen
-		if (roleId) await updatedUsers[0].$add("roles", roleId);
-
-		return res.status(200).json({
-			success: true,
-			message: "User updated successfully",
-			data: updatedUsers[0],
-		});
-	} catch (error: any) {
-		return res.status(500).json({
-			success: false,
-			message: error.message,
-			data: error,
-		});
-	}
-};
-
-export const deleteUser = async (req: any, res: any) => {
-	try {
-		const userId: string = req.params.id;
-		const user = await User.findByPk(userId);
-		if (!user) throw new Error("User not found");
-		await user.destroy();
-
-		return res.status(200).json({
-			success: true,
-			message: "User deleted successfully",
-			data: user,
-		});
-	} catch (error: any) {
-		return res.status(500).json({
-			success: false,
-			message: error.message,
-			data: error,
-		});
-	}
-};
+export const deleteUser = controllerWrapper(async (req: any, res: any) => {
+	const userId: string = req.params.id;
+	const user = await User.findByPk(userId);
+	if (!user) return responseJson(res, 404, "User not found", null);
+	await user.destroy();
+	return responseJson(res, 200, "User deleted successfully", null);
+});
