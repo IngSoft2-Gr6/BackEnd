d62672641ce04b03f74701fafaa87f17e1efe1f8
SGman98 <bastiangarnica@gmail.com>
2022-05-14 21:26:14 -0500

    Update User routes, and use bcrypt
    Add routes for register and login
    Use bcrypt for hashing password in register and comparing in login

--- src/controllers/User.controllers.ts
+++ src/controllers/User.controllers.ts
@@ -2,18 +2,30 @@ import { User } from "@models/User.model";
 import { Role } from "@models/Role.model";
 import { UserAddAttributes } from "@models/User.model";
 import { responseJson, controllerWrapper } from "@helpers/controller.helpers";
+import bcrypt from "bcrypt";
 
 export const getAllUsers = controllerWrapper(async (_req: any, res: any) => {
 	const user = await User.findAll({ include: [{ model: Role }] });
 	return responseJson(res, 200, "User retrieved successfully", user);
 });
 
-export const createUser = controllerWrapper(async (req: any, res: any) => {
+export const register = controllerWrapper(async (req: any, res: any) => {
 	const userAtt: UserAddAttributes = { ...req.body, roleId: undefined };
+	const salt = await bcrypt.genSalt(10);
+	userAtt.password = await bcrypt.hash(userAtt.password, salt);
 	const roleId: number = req.body.roleId;
 	const user = await User.create(userAtt, { include: [{ model: Role }] });
 	await user.$add("role", roleId);
 	return responseJson(res, 201, "User created successfully", user);
+});
+
+export const login = controllerWrapper(async (req: any, res: any) => {
+	const { email, password } = req.body;
+	const user = await User.findOne({ where: { email } });
+	if (!user) return responseJson(res, 404, "User not found", null);
+	const isMatch = await bcrypt.compare(password, user.password);
+	if (!isMatch) return responseJson(res, 401, "Invalid credentials", null);
+	return responseJson(res, 200, "User logged in successfully", user);
 });
 
 export const getUser = controllerWrapper(async (req: any, res: any) => {
--- src/routes/User.routes.ts
+++ src/routes/User.routes.ts
@@ -2,15 +2,20 @@ import { Router } from "express";
 import {
 	getAllUsers,
 	getUser,
-	createUser,
+	register,
+	login,
 	updateUser,
 	deleteUser,
 } from "@controllers/User.controllers";
 
 const router = Router();
 
-router.route("/").get(getAllUsers).post(createUser);
+router.route("/").get(getAllUsers);
 
 router.route("/:id").get(getUser).patch(updateUser).delete(deleteUser);
+
+router.route("/register").post(register);
+
+router.route("/login").post(login);
 
 export default router;
