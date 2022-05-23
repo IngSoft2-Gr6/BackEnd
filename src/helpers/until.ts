f06f6cbd29d32f948faafc1a5268bc69f3616c6c
Harrison <harrison.pinto@d4d.com.co>
2022-05-22 21:25:12 -0500

    Add overload signatures to `until` helper
    Co-authored-by: Sebastian Garnica <SGman98@users.noreply.github.com>

--- src/helpers/until.ts
+++ src/helpers/until.ts
@@ -1,26 +1,33 @@
-export function until(
-	promiseOrPromiseList: any[] | Promise<any>
-): Promise<any> {
+async function until<T>(
+	promiseOrPromiseList: Promise<T>
+): Promise<[any, T | undefined]>;
+
+async function until<T>(
+	promiseOrPromiseList: Promise<T>[]
+): Promise<[any, T[] | undefined[]]>;
+
+async function until<T>(
+	promiseOrPromiseList: Promise<T>[] | Promise<T>
+): Promise<[any, T | T[] | undefined | undefined[]]> {
 	if (!promiseOrPromiseList) {
 		console.error("no promise provided", promiseOrPromiseList);
-		return Promise.reject(["Unknow error"]);
+		return Promise.reject([undefined, undefined]);
 	}
 	// array of promises
 	if (Array.isArray(promiseOrPromiseList)) {
-		return Promise.all(promiseOrPromiseList)
-			.then((results) => {
-				return [null, results];
-			})
-			.catch((err) => {
-				return [err, promiseOrPromiseList.map(() => undefined)];
-			});
+		try {
+			const results = await Promise.all(promiseOrPromiseList);
+			return [null, results];
+		} catch (e) {
+			return [e, promiseOrPromiseList.map(() => undefined)];
+		}
 	}
-	// single promise
-	return promiseOrPromiseList
-		.then((result: any) => {
-			return [null, result];
-		})
-		.catch((err: any) => {
-			return [err, undefined];
-		});
+	try {
+		const result = await promiseOrPromiseList;
+		return [null, result];
+	} catch (e) {
+		return [e, undefined];
+	}
 }
+
+export default { until };
