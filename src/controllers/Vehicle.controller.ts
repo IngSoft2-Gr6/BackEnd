de5efdd6cf2278c60ad3a1be743bc6f1fae39d2f
SGman98 <bastiangarnica@gmail.com>
2022-06-18 00:39:50 -0500

    Allow vehicle creation by employee

--- src/controllers/Vehicle.controller.ts
+++ src/controllers/Vehicle.controller.ts
@@ -7,7 +7,24 @@ import until from "@helpers/until";
 export const addVehicleByDriver = async (req: any, res: any) => {
 	const user = res.locals.user as User;
 
-	const vehicleAtt: VehicleAddAttributes = { ...req.body, driverId: user.id };
+	let vehicleAtt: VehicleAddAttributes = req.body;
+	const roles = user.roles.map((role: any) => role.name);
+
+	// if roles not includes nor employee nor driver, return error
+	if (!roles.includes("Employee") && !roles.includes("Driver")) {
+		return responseJson(res, 401, "Unauthorized");
+	}
+
+	// If user has role Driver, add userId as driverId
+	if (roles.includes("Driver")) {
+		vehicleAtt = { ...vehicleAtt, driverId: user.id };
+	}
+
+	// If user has role Employee and active role current role is Employee
+	// create a vehicle with no driver
+	if (req.body.asEmployee && roles.includes("Employee")) {
+		vehicleAtt = { ...vehicleAtt, driverId: undefined };
+	}
 
 	const [err, vehicle] = await until(Vehicle.create(vehicleAtt));
 	if (err) return responseJson(res, 500, err.message);
