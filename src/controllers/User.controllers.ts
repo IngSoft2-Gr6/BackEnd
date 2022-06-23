a14ab9066cd3db057603644843ef933fe784d01e
Harrison <harrison.pinto@d4d.com.co>
2022-06-22 19:46:37 -0500

    Improve user deletion
    When user is deleted a `No content` response is sent back to the client.
    
    Set `verified` to `false` upon deletion.
    
    Co-authored-by: Sebastian Garnica <SGman98@users.noreply.github.com>

--- src/controllers/User.controllers.ts
+++ src/controllers/User.controllers.ts
@@ -216,11 +216,21 @@ export const updateUser = async (req: any, res: any) => {
 export const deleteUser = async (req: any, res: any) => {
 	const user = res.locals.user as User;
 
-	//FIXME: Verified should be set to false
-	const [err, userDeleted] = await until(user.destroy());
+	// Verified should be set to false
+	const [errorUpdating, userUpdated] = await until(
+		user.update({ verified: false })
+	);
 
-	if (err) return responseJson(res, 500, err.message);
+	if (errorUpdating) return responseJson(res, 500, errorUpdating.message);
+	if (!userUpdated) {
+		return responseJson(res, 400, "User could not be deactivated");
+	}
+
+	const [errorDeleting, userDeleted] = await until(userUpdated.destroy());
+
+	if (errorDeleting) return responseJson(res, 500, errorDeleting.message);
 	if (!userDeleted) return responseJson(res, 400, "User not deleted");
 
-	return responseJson(res, 200, "User deleted successfully", user);
+	// No further information, deleted successfully
+	return responseJson(res, 204);
 };
